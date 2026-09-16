462fbca39172e8dbb9be6048d137c0e06f666fb2
Juanpbb6811 <jberltran@ucundinamarca.edu.co>
2026-09-16 15:59:28 -0500

    controlador Historia Clinica

--- src/main/java/com/vetcare/grupo_3/Controller/historiaClinicaController.java
+++ src/main/java/com/vetcare/grupo_3/Controller/historiaClinicaController.java
@@ -1,4 +1,35 @@
 package com.vetcare.grupo_3.Controller;
 
+import com.vetcare.grupo_3.Entity.HistoriaClinica;
+import com.vetcare.grupo_3.Service.historiaClinicaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/historias-clinicas")
+@RequiredArgsConstructor
+@Tag(name = "Historias Clínicas", description = "API para la gestión de los diagnósticos y tratamientos de las mascotas")
 public class historiaClinicaController {
+
+    private final historiaClinicaService service;
+
+    @GetMapping("/mascota/{mascotaId}")
+    @Operation(summary = "Consultar la historia clínica asociada a una mascota específica")
+    public ResponseEntity<HistoriaClinica> buscarPorMascotaId(@PathVariable Long mascotaId) {
+        return ResponseEntity.ok(service.BuscarHistoriaClinicaPorMascotaId(mascotaId));
+    }
+
+    @PostMapping("/mascota/{mascotaId}")
+    @Operation(summary = "Crear el registro de historia clínica para una mascota")
+    public ResponseEntity<HistoriaClinica> crearHistoriaClinica(
+            @Valid @RequestBody HistoriaClinica historiaClinica,
+            @PathVariable Long mascotaId) {
+        // @Valid asegurará que el motivo, diagnóstico y tratamiento no vengan vacíos
+        return new ResponseEntity<>(service.crearHistoriaClinica(historiaClinica, mascotaId), HttpStatus.CREATED);
+    }
 }
